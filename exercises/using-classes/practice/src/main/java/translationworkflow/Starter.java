525a4f4e37e60580255c94b6478c076ddcf70eeb
Tom Wheeler <tom@temporal.io>
2023-10-20 16:51:18 -0500

    tab to spaces

--- exercises/using-classes/practice/src/main/java/translationworkflow/Starter.java
+++ exercises/using-classes/practice/src/main/java/translationworkflow/Starter.java
@@ -28,6 +28,6 @@ public class Starter {
     TranslationWorkflowOutput greeting = workflow.sayHelloGoodbye(input);
 
     System.out.printf("Workflow result: %s\n", greeting);
-	System.exit(0);
+    System.exit(0);
   }
 }
