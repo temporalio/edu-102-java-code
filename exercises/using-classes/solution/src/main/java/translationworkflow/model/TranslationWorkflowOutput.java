5d82caef399b48506d783bb64ebd03e8321d6f1e
Tom Wheeler <tom@temporal.io>
2023-10-19 19:11:37 -0500

    reformatted and very slight refactorings to better reflect conventions

--- exercises/using-classes/solution/src/main/java/translationworkflow/model/TranslationWorkflowOutput.java
+++ exercises/using-classes/solution/src/main/java/translationworkflow/model/TranslationWorkflowOutput.java
@@ -5,7 +5,6 @@ public class TranslationWorkflowOutput {
   private String goodbyeMessage;
 
   public TranslationWorkflowOutput() {
-
   }
 
   public TranslationWorkflowOutput(String helloMessage, String goodbyeMessage) {
